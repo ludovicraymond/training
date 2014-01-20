b8cbf3c22719ea374b4cb07d7b8cbce49ab70113
LudovicRaymond <ludo@MacBook-Air-de-ludovic-93.local>
2014-01-20 17:50:57 +0100

    Add ludo feature

--- src/Carbon/Carbon.php
+++ src/Carbon/Carbon.php
@@ -2,7 +2,7 @@
 
 /*
  * This file is part of the Carbon package.
- *
+ * Test Ludovic Feature
  * (c) Brian Nesbitt <brian@nesbot.com>
  *
  * For the full copyright and license information, please view the LICENSE
