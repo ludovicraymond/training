dc831e2f79d2b56e236f17182442a0f4212713fa
LudovicRaymond <ludo@MacBook-Air-de-ludovic-93.local>
2014-01-20 17:56:52 +0100

    Add ludo 2nd comment

--- src/Carbon/Carbon.php
+++ src/Carbon/Carbon.php
@@ -3,6 +3,7 @@
 /*
  * This file is part of the Carbon package.
  * Test Ludovic Feature
+ * My second comment
  * (c) Brian Nesbitt <brian@nesbot.com>
  *
  * For the full copyright and license information, please view the LICENSE
